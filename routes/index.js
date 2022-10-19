69868e3288b5cf8a0daf0be9c191059b0751ecaa
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-19 20:08:43 +0800

    [update]: route instead of using the /api using the / only

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 const controller = require('../controllers/index')
 
-router.get('/api',  controller.index);
+router.get('/',  controller.index);
 
-router.get('/api/delete-user',  controller.delete);
+router.get('/delete-user',  controller.delete);
 
 module.exports = router; 
