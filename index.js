b81a7275ae96911b8c7a575446aa9b3a95db7f5c
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-19 17:03:06 +0800

    update routes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const product = require('./api/product');
+const product = require('./routes');
 
 app.use(express.json({extended: false}));
 
-app.use('/api/product', product);
+// app.use('/api/product', product);
+app.use('/', product)
 
-const PORT = process.env.PORT  || 8080;
+const PORT = process.env.PORT  || 8000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 
