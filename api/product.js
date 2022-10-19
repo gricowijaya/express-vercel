cd0e73e02447f7c11c01757c612b5ec5117ba505
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-19 16:26:34 +0800

    delete controller and api folder

--- api/product.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const controller = require('../controllers/index')
-
-router.get('/', async(req, res) => { controller.index });
-
-router.get('/delete-user', async(req, res) => { controller.delete() });
-
-module.exports = router; 
